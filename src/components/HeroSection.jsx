804a39b97d8a3de4859e1e8a83ba303261872694
deepakguptabvp <deepakgupta.150743@gmail.com>
2025-06-03 15:30:09 +0530

    ui chnge

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-800 to-indigo-600 opacity-90"></div>
       </div>
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 lg:py-32">
-        <h1 className="text-3xl md:text-5xl font-extrabold text-center">
+        <h1 className="text-4xl md:text-5xl font-extrabold text-center">
           Find Your Perfect PG Accommodation
         </h1>
           <p className="mt-6 text-md text-center max-w-3xl mx-auto">
